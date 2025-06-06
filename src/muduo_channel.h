ea1afffdd4ef0c722f2cf580a5ebde796155ca81
zgz <978627093@qq.com>
2025-06-06 21:28:21 +0800

    channel

--- src/muduo_channel.h
+++ src/muduo_channel.h
@@ -21,15 +21,60 @@ public:
 
     void handleEvent(Timestamp receiveTimestamp);
 
+    void setReadCallback(ReadEventCallback cb) 
+    { 
+        readCallback_ = std::move(cb);
+    }
+
+    void setWriteCallback(EventCallback cb)
+    {
+        writeCallback_ = std::move(cb);
+    }
+
+    void setCloseCallback(EventCallback cb)
+    {
+        closeCallback_ = std::move(cb);
+    }
+
+    void setErrorCallback(EventCallback cb)
+    {
+        errorCallback_ = std::move(cb);
+    }
+
+    void tie(const std::shared_ptr<void>&);
+
+    int fd() const { return fd_; }
+    int events() const { return events_; }
+    int set_revents(int revt) { revents_ =revt; }
+    bool isNoneEvent() const { return events_ == kNoneEvent; }
+
+    void enableReading() { events_ |= kReadEvent; update(); }
+    void disableReading() { events_ &= ~kReadEvent; update(); }
+    void enableWriting() { events_ |= kWriteEvent; update(); }
+    void disableWriting() { events_ &= ~kWriteEvent; update(); }
+    void disableAll() { events_ = kNoneEvent; update(); }
+
+    bool isNoneEvent() const { return events_ == kNoneEvent; }
+    bool isWriting() const { return events_ & kWriteEvent; }
+    bool isReading() const { return events_ & kReadEvent; }
+
+    int index() {return index_; }
+    void set_index(int idx) { index_ = idx; }
+
+    EvenLoop* ownerLoop() { return loop_; }
+    void remove();
 private:
+    void update();
+    void handleEventWithGuard(Timestamp receiveTime);
+
     static const int kNoneEvent;
     static const int kReadEvent;
     static const int kWriteEvent;
 
-    EvenLoop* loop_;
-    const int fd_;
-    int events_;
-    int revents_;
+    EvenLoop* loop_; //事件循环
+    const int fd_; //poller监听对象
+    int events_; //监听的事件
+    int revents_;//poller返回的具体发生的事件
     int index_;
 
     std::weak_ptr<void> tie_;
