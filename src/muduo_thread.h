fbfb38851d0abc2a03e4635dff876db3ba6153f7
zgz <978627093@qq.com>
2025-06-16 21:46:45 +0800

    one loop per thread

---
+++ src/muduo_thread.h
@@ -0,0 +1,38 @@
+#ifndef MUDUO_THREAD_H
+#define MUDUO_THREAD_H
+
+#include "muduo_noncopyable.h"
+
+#include <functional>
+#include <thread>
+#include <memory>
+#include <atomic>
+
+class Thread : noncopyable
+{
+public:
+    using ThreadFunc = std::function<void()>;
+
+    explicit Thread(ThreadFunc, const std::string &name = std::string());
+    ~Thread();
+
+    void start();
+    void join();
+
+    bool started() const { return started_; }
+    pid_t tid() const { return tid_; }
+    const std::string& name() const { return name_; }
+
+    static int numCreated() { return numCreated_; }
+private:
+    void setDefaultName();
+
+    bool started_;
+    bool joined_;
+    std::shared_ptr<std::thread> thread_;
+    pid_t tid_;
+    ThreadFunc func_;
+    std::string name_;
+    static std::atomic_int32_t numCreated_;
+};
+#endif
